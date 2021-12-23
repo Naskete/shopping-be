6b37ef46554eac3074916e5ded89b6c7811d274c
naskete <1776795871@qq.com>
2021-12-23 16:09:20 +0800

    get user info to login

--- src/main/java/com/work/shopping/Controller/LoginController.java
+++ src/main/java/com/work/shopping/Controller/LoginController.java
@@ -2,6 +2,8 @@ package com.work.shopping.Controller;
 
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
+import com.work.shopping.Entity.User;
+import com.work.shopping.Service.UserService;
 import com.work.shopping.Utils.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +14,25 @@ public class LoginController {
     @Autowired
     private MD5Util md5Util;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/login")
-    public SaResult login(@RequestParam("username") String username,
+    public SaResult login(@RequestParam("username") String account,
                           @RequestParam("password") String password){
+//        System.out.println(account + "\t" + password);
         // 从数据库查询
-        String mdash = "ash";
+        if(account.equals("")|| password.equals("")){
+            return SaResult.error("用户名或密码不能为空");
+        }
+        User user = userService.getUserByAccount(account);
+        String mdash = user.getPassword();
+//        System.out.println("mdash = " + mdash);
         if(md5Util.checkPwd(password, mdash)){
-            StpUtil.login(username);
+            StpUtil.login(account);
             return SaResult.ok("登录成功");
         }
-        return SaResult.error("登录失败");
+        return SaResult.error("登录失败，用户名或密码错误");
     }
 
     @GetMapping("/logout")
