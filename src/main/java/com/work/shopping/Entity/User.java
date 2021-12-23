ab5237e1c92afafab11a5a6ecbddba26b7c458e7
naskete <1776795871@qq.com>
2021-12-23 15:28:34 +0800

    complete user

--- src/main/java/com/work/shopping/Entity/User.java
+++ src/main/java/com/work/shopping/Entity/User.java
@@ -6,9 +6,67 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
-    @Column(name = "id")
+    @Column(name = "account")
     private String id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "grade")
+    private int grade;
+    @Column(name = "remain")
+    private float remain;
+    @Column(name = "vip")
+    private int vip;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public float getRemain() {
+        return remain;
+    }
+
+    public void setRemain(float remain) {
+        this.remain = remain;
+    }
+
+    public int getVip() {
+        return vip;
+    }
+
+    public void setVip(int vip) {
+        this.vip = vip;
+    }
 }
