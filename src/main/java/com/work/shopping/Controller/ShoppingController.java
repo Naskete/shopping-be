f343b40afb34737dd114369105e6cca7cf445f6d
naskete <1776795871@qq.com>
2021-12-23 16:36:27 +0800

    add purchase method

--- src/main/java/com/work/shopping/Controller/ShoppingController.java
+++ src/main/java/com/work/shopping/Controller/ShoppingController.java
@@ -1,13 +1,12 @@
 package com.work.shopping.Controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import com.work.shopping.Service.ShoppingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
 import java.util.List;
 @CrossOrigin
 @RestController
@@ -19,7 +18,20 @@ public class ShoppingController {
     public SaResult findAll(){return new SaResult(200,"successful",shoppingService.getProducts());}
 
     @GetMapping("/product/{id}")
-    public SaResult getProductByID(@PathVariable("id")String id){
+    public SaResult getProductByID(@PathVariable("id") BigInteger id){
         return new SaResult(200,"successful",shoppingService.getProductById(id));
+    }
+
+    @PostMapping("/puschase")
+    public SaResult purchase(String productId){
+        // 判断登录状态
+        if(!StpUtil.isLogin()){
+            return SaResult.error("请登录");
+        }
+        // 获取当前用户account
+        String account = StpUtil.getLoginId().toString();
+        System.out.println(account);
+        shoppingService.purchase(account, productId);
+        return SaResult.ok("ok");
     }
 }
