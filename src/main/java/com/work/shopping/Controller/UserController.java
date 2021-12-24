473a1c970c0cf8fcb6d9ff9b55c3b2b6effb4824
Nick <1285817563@qq.com>
2021-12-24 18:10:47 +0800

    add list,registerUser,updateUser

--- src/main/java/com/work/shopping/Controller/UserController.java
+++ src/main/java/com/work/shopping/Controller/UserController.java
@@ -1,12 +1,13 @@
 package com.work.shopping.Controller;
 
 import cn.dev33.satoken.util.SaResult;
+import com.work.shopping.Entity.User;
 import com.work.shopping.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 
 @CrossOrigin
@@ -15,8 +16,45 @@ public class UserController  {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user/{id}")
-    public SaResult getUserByAccount(@PathVariable("id") String id){
-        return new SaResult(200,"successful", userService.getUserByAccount(id));
+    /*
+    * 将所有user信息返回到list界面下
+    * */
+    @RequestMapping("/users")
+    public String list(Model model){
+        Collection<User> users = userService.getAll();
+        model.addAttribute("users",users);
+        return "user/list";
     }
+
+    /*
+    * 将注册用户的信息保存到数据库，返回login界面
+    * */
+    @PostMapping("/registerUser")
+    public String registerUser(User user){
+        userService.addUser(user);
+        return "redirect:/login";
+    }
+
+    /*
+    * 前往修改用户信息的界面user/add
+    * */
+    @GetMapping("/user/{account}")
+    public String toUpdateUser(@PathVariable("account") String account, Model model){
+        // 查出原来的数据
+        User user = userService.getUserByAccount(account);
+        model.addAttribute("user",user);
+        return "user/add";
+    }
+
+    @PostMapping("updateUser")
+    public SaResult updateUser(User user){
+        userService.addUser(user);
+        return SaResult.ok("修改成功");
+    }
+
+
+
+
+
+
 }
